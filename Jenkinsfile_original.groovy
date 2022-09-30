7d4fa653ee1dd6185a6f5ed77adaa51ee752f9dc
Somasekhar Kuruva <somasekhar.kuruva@trianz.com>
2022-09-30 07:44:08 +0530

    added checkout and build steps

--- Jenkinsfile_original.groovy
+++ Jenkinsfile_original.groovy
@@ -5,11 +5,16 @@ pipeline {
         stage("Checkout"){
             steps {
                 echo "Clone the the source code"
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/KuruvaSomaSekhar/Sep22-code.git']])
+                sh "ls -la"
             }
         }
         stage("Build"){
             steps{
                 echo "Here we build source code"
+                sh '''mvn clean package
+                ls -la target/
+                    '''
             }
         }
         stage("Upload artifacts"){
